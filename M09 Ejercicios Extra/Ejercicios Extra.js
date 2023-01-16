84f3d642cac64dcef4b67f3f9c1774b31f42223b
Zolouz966 <zrodrigo96@gmail.com>
2023-01-16 13:04:17 -0400

    Realizamos los Ejercicios Extras del Prepcourse y lo terminamos

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,11 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var array = [];
+   for (prop in objeto){
+      array.push([prop, objeto[prop]])
+   }
+   return array;
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +19,15 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   let objeto = {};
+   for (let i = 0; i < string.length; i++){
+      if(objeto.hasOwnProperty(string[i])){
+         objeto[string[i]] = objeto[string[i]] + 1 ;
+      } else {
+         objeto[string[i]] = 1 ;
+      }
+   };
+   return objeto;
 }
 
 function capToFront(string) {
@@ -22,6 +36,16 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   let mayus = '';
+   let minus = '';
+   for ( i = 0; i < string.length; i++ ){
+      if (string[i] === string[i].toUpperCase()){
+         mayus = mayus + string[i];
+      } else {
+         minus = minus + string[i];
+      }
+   };
+   return mayus + minus;
 }
 
 function asAmirror(frase) {
@@ -29,18 +53,33 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var newFrase = [ ];
+   for( var i = 0; i < frase.length; i++ ){
+      newFrase.unshift(frase[i])
+   }
+
+   return newFrase.join('').split(' ').reverse().join(' ');
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   var read = numero.toString().split('').reverse().join('');
+   var resultado = parseInt(read);
+
+   if (numero === resultado){
+      return 'Es capicua';
+   } else return 'No es capicua';
+
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   return  versionModificada = string.split('').filter((e) =>  e !== 'a' && e !== 'b' && e !== 'c').join('');
+   
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +88,24 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   // return  arrayOfStrings.sort(function(a,b){
+   //    if(a.length > b.length){
+   //       return 1;
+   //    } else if (a.length < b.length){
+   //       return -1;
+   //    } else return 0;
+   // })
+
+ for (var i = 0; i < arrayOfStrings.length; i++ ){
+      for (var j = i + 1; j < arrayOfStrings.length; j++) {
+         if(arrayOfStrings[i].length > arrayOfStrings[j].length){
+            let aux = arrayOfStrings[i];
+            arrayOfStrings[i] = arrayOfStrings[j];
+            arrayOfStrings[j] = aux
+         }
+      }
+  }
+  return arrayOfStrings
 }
 
 function buscoInterseccion(array1, array2) {
@@ -57,7 +114,16 @@ function buscoInterseccion(array1, array2) {
    // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
-   // Tu código:
+   // Tu código: 
+   let nuevoArray = [];
+    for ( let i = 0; i < array1.length; i++){
+      for ( let j = 0; j < array2.length; j++){
+         if (array1[i] === array2[j]){
+            nuevoArray.push(array1[i])
+         }
+      }
+    }
+    return nuevoArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
